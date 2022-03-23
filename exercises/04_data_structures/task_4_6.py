2e5185a52324aa5660a2270112dfce2d1fa1c305
SergeyVolkoff <SergeyVolkoff.vol7745@yandex.ru>
2022-03-23 23:02:35 +0300

    23.03.22 23:00

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -18,3 +18,6 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+out = "\n{:25} {}" *2
+b = out.format ('Prefix','route','Prefix','route')
+print (b)
