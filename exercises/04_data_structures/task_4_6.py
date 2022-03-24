b85b759829092283de8c52d6cbcc1fe11a67cafe
SergeyVolkoff <SergeyVolkoff.vol7745@yandex.ru>
2022-03-24 21:57:12 +0300

    240322_21:55

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -18,6 +18,8 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
-out = "\n{:25} {}" *2
-b = out.format ('Prefix','route','Prefix','route')
-print (b)
+ospf_route = ospf_route.replace(","," ").replace("["," ").replace("]"," ").replace("via"," ")
+ospf_route = ospf_route.split()
+out = "\n{:20} {}" *5
+out = out.format ('Prefix', ospf_route[0],'AD/Metric', ospf_route[1],'Next-Hop', ospf_route[2],'Last update', ospf_route[3],'Outbound Interface', ospf_route[4])
+print (out)
--- exercises/04_data_structures/task_4_7.py
+++ exercises/04_data_structures/task_4_7.py
@@ -16,3 +16,8 @@
 """
 
 mac = "AAAA:BBBB:CCCC"
+mac = mac.replace(":"," ")
+mac = mac.split()
+print(mac)
+mac = bin(int(mac[0],16)), bin(int(mac[1],16)), bin(int(mac[2],16))
+print(mac)
