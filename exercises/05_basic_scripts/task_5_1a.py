7c94d29479ad9204cf585741ae2cfbd56c528a12
SergeyVolkoff <SergeyVolkoff.vol7745@yandex.ru>
2022-03-28 20:38:34 +0300

    280322_20:38

--- exercises/05_basic_scripts/task_5_1a.py
+++ exercises/05_basic_scripts/task_5_1a.py
@@ -18,7 +18,8 @@ $ python task_5_1a.py
 Все задания надо выполнять используя только пройденные темы. То есть эту задачу можно
 решить без использования условия if.
 """
-
+device = input("Введите имя устройства: ")
+name_in = input("Введите имя параметра: ")
 london_co = {
     "r1": {
         "location": "21 New Globe Walk",
@@ -44,3 +45,4 @@ london_co = {
         "routing": True,
     },
 }
+print (london_co[device] [name_in])
