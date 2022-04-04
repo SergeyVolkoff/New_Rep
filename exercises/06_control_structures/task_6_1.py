b3a51d653c8b2c6ea9146705ec77b9210efd8130
SergeyVolkoff <vol7745@yandex.ru>
2022-04-04 17:55:48 +0300

    040422_18:00

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -13,3 +13,7 @@ MAC-адреса используются в формате XXXX.XXXX.XXXX
 """
 
 mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+result=[]
+for m in mac:
+    result.append(m.replace(':','.'))
+print(result)
