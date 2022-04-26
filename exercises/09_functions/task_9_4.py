e989d5690f76b96b6cc0b34ba720906ccb35b92e
SergeyVolkoff <vol7745@yandex.ru>
2022-04-26 13:57:17 +0300

    9_4

--- exercises/09_functions/task_9_4.py
+++ exercises/09_functions/task_9_4.py
@@ -64,3 +64,23 @@ def ignore_command(command, ignore):
         if word in command:
             ignore_status = True
     return ignore_status
+
+def convert_config_to_dict(config_filename):
+    config_dict = {}
+    with open(config_filename) as f:
+        for line in f:
+            line = line.rstrip()
+            if line and not (line.startswith("!") or ignore_command(line, ignore)):
+                if line[0].isalnum():
+                    section = line
+                    config_dict[section] = []
+                else:
+                    config_dict[section].append(line.strip())
+        return config_dict
+
+
+
+
+
+
+
