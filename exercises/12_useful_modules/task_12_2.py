56f65250ddc7c4be5d065c75da79b769d3131680
SergeyVolkoff <SergeyVolkoff.vol7745@yandex.ru>
2022-05-28 15:16:49 +0300

    12_2

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -34,3 +34,21 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+import ipaddress
+
+def convert_ranges_to_ip_list (ip_addresses):
+    ip_list =[]
+    for ip_address in ip_addresses:
+        if "-" in ip_addresses:
+            start_ip, end_ip = ip_addresses.split("-")
+            if "." not in end_ip:
+                end_ip = ".".join(start_ip.split(".")[:-1]+[end_ip])
+            start_ip = ipaddress.ip_address(start_ip)
+            end_ip = ipaddress.ip_address(end_ip)
+            for ip in range(int(start_ip), int(end_ip) + 1):
+                ip_list.append(str(ipaddress.ip_address(ip)))
+        else:
+              ip_list.append(str(ip_address))
+    return ip_list
+    print (start_ip, end_ip)
+
