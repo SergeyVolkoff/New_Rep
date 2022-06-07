ebbec83b6b47a894528852cfdd1d881ee1cfc328
SergeyVolkoff <vol7745@yandex.ru>
2022-06-07 13:52:29 +0300

    15_1a

--- exercises/15_module_re/task_15_1a.py
+++ exercises/15_module_re/task_15_1a.py
@@ -24,3 +24,13 @@
 а не ввод пользователя.
 
 """
+import re
+
+
+def get_ip_from_cfg (file_conf):
+    with open (file_conf) as f:
+        regex = re.compile(r"interface (?P<intf>\S+)\n( .*\n)* ip address (?P<ip>\S+) (?P<mask>\S+)")
+        match = regex.finditer(f.read())
+        result = {m.group("intf") : m.group("ip","mask") for m in match}
+        return result
+
