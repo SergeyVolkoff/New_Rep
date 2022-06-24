7a39516032d9d85bc8bd6b6f9275ddf0b975d22a
SergeyVolkoff <vol7745@yandex.ru>
2022-06-24 11:42:41 +0300

    17_3

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,25 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+import re
+import csv
+
+def parse_sh_cdp_neighbors(cfg_neib):
+    regex = re.compile(
+    r"(?P<nei_dev>\w+)  +(?P<loc_intf>\S+ \S+)"
+    r"  +\d+  +(\w ){3} +\S+ +(?P<nei_intf>\S+ \S+)"
+    )
+    result={}
+    dev=re.search(r"(\S+)[>]", cfg_neib).group(1)
+
+    result[dev]={}
+    match = re.search(regex, cfg_neib)
+    for match in regex.finditer(cfg_neib):
+        nei_dev, loc_intf, nei_intf = match.group("nei_dev", "loc_intf", "nei_intf")
+        result[dev][loc_intf] ={nei_dev : nei_intf}
+
+    return result
+
+if __name__=="__main__":
+    with open("sh_cdp_n_sw1.txt")  as f:
+     print(parse_sh_cdp_neighbors(f.read()))
