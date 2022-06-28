7e682807e888c0fb7ae23a61d29977ad6d4a7d75
SergeyVolkoff <vol7745@yandex.ru>
2022-06-29 00:07:04 +0300

    29_06_00-00

--- Avtomat/BM10/cfg_wwan0_BM10.py
+++ Avtomat/BM10/cfg_wwan0_BM10.py
@@ -1,17 +1,40 @@
 
-
+import re
 import pexpect
 from pprint import pprint
+from show_BM10 import send_show_ifcof
 
-def cfg_wwan_BM10(
-    int_name,
-    proto_value,
-    device_value,
-    apn_value,
-    pdp_type
-):
-    output_show_ifcfg = send_show_ifcof("192.168.1.1", "root", "root", ["ifconfig | grep -A 1  wwan0"])
-    if "ip" in output_show_ifcfg:
 
-        pprint({int_name}"already exists")
+
+
+def cfg_wwan_BM10(user,ip, password,cfg_commands):
+    output = send_show_ifcof("192.168.2.1", "root", "128500", ["uci show network | grep 34G"])
+    for cmd in cfg_commands:
+        if "34G" in output:
+            pprint(" int already exists")
+        else:
+            with pexpect.spawn(f"ssh {user}@{ip}", timeout=10, encoding="utf-8") as ssh:
+                ssh.expect("d:")
+                ssh.sendline(password)
+                ssh.expect("root@BWOS:~#")
+                cfg_commands = [*cfg_commands]
+                ssh.sendline(cmd)
+                ssh.expect("root@BWOS:~#")
+                ssh.sendline("uci show network | grep 34G")
+                temp = ssh.before
+                print(temp)
+                if "ip" in output:
+                    pprint(" int greate")
+
+
+if __name__ == "__main__":
+    cfg_commands = [
+    "uci set network.34G=interface",
+    "uci set network.34G.proto='qmi'",
+    "uci set network.34G.apn='internet.tele2.ru'",
+    "uci set network.34G.pdptype='ipv4'",
+    "uci commit"
+    ]
+    out = cfg_wwan_BM10("root", "192.168.2.1", "128500", cfg_commands)
+    print(out)
 
--- Avtomat/BM10/show_BM10.py
+++ Avtomat/BM10/show_BM10.py
@@ -10,7 +10,6 @@ def send_show_ifcof(ip, user, password, commands):#f-show all ifconf
     try:
         with pexpect.spawn(f"ssh {user}@{ip}", timeout=10, encoding="utf-8") as ssh:
             ssh.expect("d:")
-
             ssh.sendline(password)
             ssh.expect("root@BWOS:~#")
             if type(commands)== str:
@@ -19,13 +18,13 @@ def send_show_ifcof(ip, user, password, commands):#f-show all ifconf
                 ssh.sendline(cmd)
                 ssh.expect("root@BWOS:~#")
                 temp = ssh.before
-
-                output = re.search(r'addr:(\S+)', temp).group()
-
+                if "addr:" in temp:
+                    output = re.search(r'addr:(\S+)', temp).group()
+                else:
+                    print("no int")
         return output
-
     except pexpect.exceptions.TIMEOUT as error:
         print(f"error connect to {ip}")
 
 if __name__=="__main__":
-    pprint(send_show_ifcof("192.168.1.1", "root", "root", ["ifconfig | grep -A 2  wwan0",]),width=120)
+    pprint(send_show_ifcof("192.168.2.1", "root", "128500", ["ifconfig | grep -A 2  wlan0",]),width=120)
