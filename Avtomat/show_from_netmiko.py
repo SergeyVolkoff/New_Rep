e5f7f9e7e45c0ba96f046f301467e869e4446e48
SergeyVolkoff <vol7745@yandex.ru>
2022-07-05 04:05:04 -0400

    sh_netm

---
+++ Avtomat/show_from_netmiko.py
@@ -0,0 +1,20 @@
+
+import yaml
+import netmiko
+from netmiko import ConnectHandler
+
+def send_show_command(device, command):
+    result = ''
+    with ConnectHandler(**device) as ssh:
+        result = ssh.send_command(command)
+    return result
+
+
+
+if __name__ == "__main__":
+    command = "ifconfig"
+    with open("BM10_LTE.yaml")as f:
+        device = yaml.safe_load(f)
+
+    for dev in device:
+        print(send_show_command(dev, command))
