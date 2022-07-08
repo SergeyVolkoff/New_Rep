64ff139a03362b0b8841ff7f2d8cbbaef714e4d8
SergeyVolkoff <vol7745@yandex.ru>
2022-07-08 09:46:39 -0400

    ping_ok

--- Avtomat/BM10/ping.py
+++ Avtomat/BM10/ping.py
@@ -19,12 +19,12 @@ def ping_ip_3G (device,commands, ip_dest, promo,log = True):
                 output = ssh.send_command(temp)
                 if "round-trip min/avg/max" in output:
                     output = re.search(r'round-trip min/avg/max = (\S+ ..)', output).group()
-                    result += output
-                    print("*"*30)
-                    print("IP ",ip_dest, "destination  available from 3G: ",result)
+                    result = ("IP ",ip_dest, "destination  available from 3G: ",output)
+
+
                 else:
-                    print("*"*30)
-                    print("Ip ",ip_dest, "out of destination")
+
+                    result =("Ip ",ip_dest, "out of destination")
         return result
     except (NetmikoAuthenticationException, NetmikoTimeoutException) as error:
         print("*"*5, "Error connection to:", device['host'], "*"*5)
@@ -33,7 +33,6 @@ if __name__ == "__main__":
     promo= " -w 4"
     commands ="ping "
     temp = (commands+ip_dest+promo)
-    print(temp)
     with open("BM10_LTE.yaml")as f:
         device = yaml.safe_load(f)
         for dev in device:
