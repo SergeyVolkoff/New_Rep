5b1229567f913bba19ab27b95902e7269f435dd2
SergeyVolkoff <vol7745@yandex.ru>
2022-07-21 22:45:17 +0300

    20_4

--- exercises/20_jinja2/task_20_1.py
+++ exercises/20_jinja2/task_20_1.py
@@ -33,8 +33,8 @@ def generate_config(template, data_dict):
 
 # так должен выглядеть вызов функции
 if __name__ == "__main__":
-    data_file = "data_files/router_info.yml"
-    template_file = "templates/cisco_router_base.txt"
+    data_file = "data_files/add_vlan_to_switch.yaml"
+    template_file = "templates/add_vlan_to_switch.txt"
     with open(data_file) as f:
         data = yaml.safe_load(f)
     print(generate_config(template_file, data))
