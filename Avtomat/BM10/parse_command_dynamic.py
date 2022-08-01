53d0f73a0c49ee9b7049a3aaf2f9e186508ffd77
SergeyVolkoff <vol7745@yandex.ru>
2022-08-01 08:33:29 -0400

    123

--- Avtomat/BM10/parse_command_dynamic.py
+++ Avtomat/BM10/parse_command_dynamic.py
@@ -2,18 +2,27 @@ import yaml
 import textfsm
 from sh_base_cfg_BM10 import sh_base_cfg_BM10
 from textfsm import clitable
-
+import netmiko
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
 def parse_command_dynamic(command_output, atributes_dict, index_file = "index", templ_path = "templates"):
     clitable1 = clitable.CliTable(index_file, templ_path)
     clitable1.ParseCmd(command_output, atributes_dict)
-    return [ item for item in clitable1]
+    header = list(clitable1.header)
+    data = clitable1.FormattedTable()
+    return header, data
+
+
 if __name__ == "__main__":
-    command = ["arp -a"]
     atributes = {"Command":"arp -a", "Vendor":"linux_os"}
+    command = ["arp -a"]
     with open("BM10_LTE.yaml") as f:
         device = yaml.safe_load(f)
         for dev in device:
             output = sh_base_cfg_BM10(dev, command)
-
-            result_clitable = parse_command_dynamic(output, atributes)
+            print(output)
+    result_clitable = parse_command_dynamic(output, atributes)
     print (result_clitable)
