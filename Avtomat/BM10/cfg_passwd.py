70a41898089f9e05078e330c06d6db0afa004005
SergeyVolkoff <vol7745@yandex.ru>
2022-08-09 11:03:05 -0400

    sdv

--- Avtomat/BM10/cfg_passwd.py
+++ Avtomat/BM10/cfg_passwd.py
@@ -17,17 +17,23 @@ def cfg_pass (device, commands, log = True):
         with ConnectHandler(**device) as ssh:
             print(device['host'], "connected")
             for comm in commands:
-                temp = ssh.send_command(comm, expect_string="password:")
-                result [comm] = temp
+                temp = ssh.send_command(command_string="passwd",expect_string= "New password:")
+                print("!", temp)
+                prompt = ssh.find_prompt()
+                temp1=ssh.send_command(comm,expect_string=prompt)
+                print("@", temp1)
+                prompt = ssh.find_prompt()
+                temp2 = ssh.send_command(comm, expect_string=prompt)
+                print("#", temp2)
+                prompt = ssh.find_prompt()
+                temp3 = ssh.send_command(comm, expect_string=prompt)
+                print("$", temp3)
+            result = temp
         return result
     except (NetmikoAuthenticationException, NetmikoTimeoutException) as error:
         print("*"*5, "Error connection to:", device['host'], "*"*5)
 if __name__== "__main__":
-    commands = [
-        "passwd",
-    "root12",
-        "root12"
-]
+    commands = ["root12","root12","root12"]
     with open("BM10_LTE.yaml") as f:
         device = yaml.safe_load(f)
         for dev in device:
