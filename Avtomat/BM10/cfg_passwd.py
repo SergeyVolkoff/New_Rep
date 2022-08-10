07097c9533dfb028176761c05f60ceb134a2faf7
SergeyVolkoff <vol7745@yandex.ru>
2022-08-10 11:01:38 -0400

    asd

--- Avtomat/BM10/cfg_passwd.py
+++ Avtomat/BM10/cfg_passwd.py
@@ -16,24 +16,27 @@ def cfg_pass (device, commands, log = True):
     try:
         with ConnectHandler(**device) as ssh:
             print(device['host'], "connected")
-            for comm in commands:
-                temp = ssh.send_command(command_string="passwd",expect_string= "New password:")
-                print("!", temp)
-                prompt = ssh.find_prompt()
-                temp1=ssh.send_command(comm,expect_string=prompt)
-                print("@", temp1)
-                prompt = ssh.find_prompt()
-                temp2 = ssh.send_command(comm, expect_string=prompt)
-                print("#", temp2)
-                prompt = ssh.find_prompt()
-                temp3 = ssh.send_command(comm, expect_string=prompt)
-                print("$", temp3)
-            result = temp
-        return result
+
+            temp=ssh.send_command(command_string="passwd", expect_string=" ")
+            prompt = ssh.find_prompt()
+            pattern = ssh.read_until_pattern(prompt)
+            print("~",pattern)
+            print("!",temp)
+
+            temp1 = ssh.send_command(command_string="12345678", expect_string=" ")
+            print("@",temp1)
+
+            temp2 = ssh.send_command(command_string="12345678", expect_string=" ")
+            print("#", temp2)
+
+            temp3 = ssh.send_command(command_string="12345678", expect_string=" ")
+            print("$", temp3)
+
+
     except (NetmikoAuthenticationException, NetmikoTimeoutException) as error:
         print("*"*5, "Error connection to:", device['host'], "*"*5)
 if __name__== "__main__":
-    commands = ["root12","root12","root12"]
+    commands = ["root12","root12","root12","root12"]
     with open("BM10_LTE.yaml") as f:
         device = yaml.safe_load(f)
         for dev in device:
--- Avtomat/BM10/clss_ssh_connect.py
+++ Avtomat/BM10/clss_ssh_connect.py
@@ -5,22 +5,20 @@ from netmiko import (
     NetmikoTimeoutException,
     NetmikoAuthenticationException,
 )
-
 class BwosSSH:
     def __init__(self, device_type, host, username, password, timeout):
         self.ssh  = ConnectHandler(**device)
-
+        self.ip = host
+        self.name = username
+        self.passwd = password
     def send_sh_command(self,command):
         temp = self.ssh.send_command(command)
         result = temp
         return result
 if __name__ == "__main__":
-    device = {
-        "device_type": "linux",
-        "host": "192.168.2.1",
-        "username": "root",
-        "password": "!128500",
-        "timeout": "1"
-    }
-    r1 = BwosSSH(**device)
-    print (r1.send_sh_command("uci show"))
+    with open("BM10_LTE.yaml") as f:
+        temp = yaml.safe_load(f)
+        for t in temp:
+            device = dict(t)
+            r1 = BwosSSH(**device)
+            print (r1.send_sh_command("uci show network"))
