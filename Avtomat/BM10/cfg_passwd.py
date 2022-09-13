e749569df28b7aaa8a4807e3a1358cee2834043f
SergeyVolkoff <vol7745@yandex.ru>
2022-09-13 07:39:12 -0400

    clss_R

--- Avtomat/BM10/cfg_passwd.py
+++ Avtomat/BM10/cfg_passwd.py
@@ -10,29 +10,30 @@ from netmiko import *
 
 
 def cfg_pass (device, commands, log = True):
+    new_pass = input("Input new pass: ")
     if log:
         print(f"Connect to {device['host']}...")
     result = ''
     try:
         with ConnectHandler(**device) as ssh:
-            for comm in commands:
-                prompt = ssh.find_prompt()
-                output = ssh.send_command(comm, expect_string="New password:", read_timeout=1)
+            prompt = ssh.find_prompt()
+            output = ssh.send_command(commands, expect_string="New password:", read_timeout=1)
+            print(output, "****")
+            if "New" in output:
+                output = ssh.send_command_timing(new_pass, read_timeout=1)
                 print(output, "****")
-                if "New" in output:
-                    output = ssh.send_command_timing("root",  read_timeout=1)
-                    print(output, "****")
-                    if "Re-enter new password:" in output:
-                        output = ssh.send_command_timing("root",  read_timeout=1)
-                        print(output)
-                        while True :
-                            if "root@" not in output :
-                                output = ssh.read_until_pattern(f'{prompt}', read_timeout=0)
-                                print("Wait, the password will change now")
-                                ssh.write_channel(" ")
-                            elif "root@" in output:
-                                print("New pass OK")
-                                break
+                if "Re-enter new password:" in output:
+                    output = ssh.send_command_timing(new_pass, read_timeout=1)
+                    print(output)
+                    while True:
+                        if "root@" not in output:
+                            output = ssh.read_until_pattern(f'{prompt}', read_timeout=0)
+                            print("Wait, the password will change now")
+                            ssh.write_channel(" ")
+                        elif "root@" in output:
+                            print("New pass OK")
+                            break
+
         return output
     except (NetmikoAuthenticationException, NetmikoTimeoutException) as error:
         print("*" * 5, "Error connection to:", device['host'], "*" * 5)
@@ -40,10 +41,8 @@ def cfg_pass (device, commands, log = True):
 
 
 if __name__== "__main__":
-    commands = [
-        "passwd"
-    ]
+
     with open("BM10_LTE.yaml") as f:
         device = yaml.safe_load(f)
         for dev in device:
-                cfg_pass(dev, commands)
+                cfg_pass(dev, commands="passwd")
--- Avtomat/BM10/clss_Router.py
+++ Avtomat/BM10/clss_Router.py
@@ -29,14 +29,14 @@ class Router:
         except(NetmikoAuthenticationException,NetmikoTimeoutException) as error:
             print("*" * 5, "Error connection to:", device['host'], "*" * 5)
     """
-    ф-я отправки простой команды в уст-во по ssh, без импорта
+    ФУНКЦИЯ отправки простой команды в уст-во по ssh, без импорта
     """
     def send_sh_command(self, command):
         temp = self.ssh.send_command(command)
         result = temp
         return result
     """
-     Ф-я изменения пароля, надо поменять так, 
+     ФУНКЦИЯ изменения пароля, надо поменять так, 
      чтоб можно было вводить короткий пароль без сбоя и тащить пароль со стороны, а не из кода.
      без импорта
     """
@@ -46,29 +46,35 @@ class Router:
         result = ''
         try:
             with ConnectHandler(**device) as ssh:
-                for comm in commands:
-                    prompt = ssh.find_prompt()
-                    output = ssh.send_command(comm, expect_string="New password:", read_timeout=1)
+                new_pass = input("Input new pass: ")
+                prompt = ssh.find_prompt()
+                output = ssh.send_command(commands, expect_string="New password:", read_timeout=1)
+                print(output, "****")
+                if "New" in output:
+                    output = ssh.send_command_timing(new_pass, read_timeout=1)
                     print(output, "****")
-                    if "New" in output:
-                        output = ssh.send_command_timing("Qq123456", read_timeout=1)
-                        print(output, "****")
-                        if "Re-enter new password:" in output:
-                            output = ssh.send_command_timing("Qq123456", read_timeout=1)
-                            print(output)
-                            while True:
-                                if "root@" not in output:
-                                    output = ssh.read_until_pattern(f'{prompt}', read_timeout=0)
-                                    print("Wait, the password will change now")
-                                    ssh.write_channel(" ")
-                                elif "root@" in output:
-                                    print("New pass OK")
-                                    break
+                    if "Bad password" not in output:
+                        pass
+                    else:
+                        output = ssh.send_command_timing(new_pass, read_timeout=1)
+                        print(output)
+                    if "Re-enter new password:" in output:
+                        output = ssh.send_command_timing(new_pass, read_timeout=1)
+                        print(output)
+                        while True:
+                            if "root@" not in output:
+                                output = ssh.read_until_pattern(f'{prompt}', read_timeout=0)
+                                print("Wait, the password will change now")
+                                ssh.write_channel(" ")
+                            elif "root@" in output:
+                                print("New pass OK")
+                                break
+
             return output
         except (NetmikoAuthenticationException, NetmikoTimeoutException) as error:
             print("*" * 5, "Error connection to:", device['host'], "*" * 5)
     """
-    Функция для простого пинга, сама запросит адрес назначения, формат команды прописан в инит.
+    ФУНКЦИЯ для простого пинга,  запросит адрес назначения, формат команды прописан в инит.
     без импорта.
     """
     def ping_ip(self, device, command_ping):
@@ -85,14 +91,14 @@ class Router:
             result = ' '.join(result)
         return result
     """
-    ф-я сброса конфига на заводской, с ребутом устр-ва. 
+    ФУНКЦИЯ сброса конфига на заводской, с ребутом устр-ва. 
     без импорта
     """
     def reset_conf(self,device, comm_reset_conf):
         result_reset=self.ssh.send_config_set(self.commands_to_reset_conf)
         return result_reset
     """
-    ф-я настройки 3G, с ребутом уср-ва.
+    ФУНКЦИЯ настройки 3G, с ребутом уср-ва.
     без импорта
     """
     def cfg_LTE(self, device, command_cfg_3G):
@@ -100,6 +106,7 @@ class Router:
             output = self.ssh.send_config_set(comm)
             print (output)
     """
+    ФУНКЦИЯ просмотра интерфейса 3G
      Доделать!
     """
 
@@ -132,7 +139,10 @@ if __name__ == "__main__":
         for t in temp:
             device = dict(t)
             r1 = Router(**device)
+
             #print(r1.ping_ip(device,r1.command_ping ))
-            # print(r1.reset_conf(device,r1.commands_to_reset_conf)
+            #print(r1.reset_conf(device,r1.commands_to_reset_conf))
             #print(r1.cfg_LTE(device,r1.commands_cfg_3G))
             print(r1.show_int3G(device,"uci show network | grep 34G"))
+            #print(r1.cfg_pass(device,commands="passwd"))
+            #print(r1.cfg_LTE(device,r1.commands_cfg_3G))
