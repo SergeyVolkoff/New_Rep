d42804956713575f5bf640c74d32c541b5dad1c9
SergeyVolkoff <vol7745@yandex.ru>
2022-10-27 23:06:13 +0300

    sh_base

--- Avtomat/BM10/sh_base_cfg_BM10.py
+++ Avtomat/BM10/sh_base_cfg_BM10.py
@@ -11,6 +11,7 @@ from netmiko import (
 from rich.table import Table
 from rich.console import Console
 from rich.theme import Theme
+import json  # для преобразования строки в словарь
 
 
 my_colors = Theme( #добавляет цветовую градацию для rich
@@ -26,18 +27,26 @@ def sh_base_cfg_BM10(device, commands,log = True):
         print(f"Connect to {device['host']}...")
 
     try:
-        result={}
+
         with ConnectHandler(**device) as ssh:
             console.print(device['host'], "connected",style='success')
             for command in commands:
+                result = {}
                 output = ssh.send_command(command)
 
-                match = re.search(r'(?P<comm_name>(\S+))=(?P<out>\S+)', output)
-                if match == None:
-                    result[command] = "bad comm\nor\nnot int"
-                else:
-                    result[match.group('comm_name')]=(match.group('out'))
-                print(result)
+
+                output = output.replace('=', '" : "')
+                output = output.replace('\n', '",\n"')
+                output = ('"' + output+ '"')
+                print(output)
+
+
+                #result = {s.split('=')[0]: s.split()[1:] for s in output}
+
+                # for s in output:
+                #     tmp=s.split('=')
+                #     result[tmp[0]]=tmp[1:]
+                #print(result)
         c = Console()
         table = Table(show_lines=True)
         for r in "command output".split():
@@ -53,16 +62,16 @@ def sh_base_cfg_BM10(device, commands,log = True):
 
 if __name__ == "__main__":
     commands = [
-    "uci show system.@system[0].hostname",
-    "uci show firewall.@zone[1].input",
-    "uci show firewall.@zone[1].output",
-    "uci show firewall.@zone[1].forward",
-    "uci show firewall.@defaults[0].flow_offloading",
-    "uci show firewall.@defaults[0].flow_offloading_hw",
-    "uci show wireless.default_radio0.ssid",
-    "uci show network.wan",
-    "uci show network.lan",
-    "uci show network.@route[0]"
+    # "uci show system.@system[0].hostname",
+    # "uci show firewall.@zone[1].input",
+    # "uci show firewall.@zone[1].output",
+    # "uci show firewall.@zone[1].forward",
+    # "uci show firewall.@defaults[0].flow_offloading",
+    # "uci show firewall.@defaults[0].flow_offloading_hw",
+    # "uci show wireless.default_radio0.ssid",
+    "uci show network",
+    # "uci show network.lan",
+    # "uci show network.@route[0]"
     ]
     with open("BM10_LTE.yaml")as f:
         device = yaml.safe_load(f)
