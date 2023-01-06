dc01a8cd483ba1c55ee2c4c12064a0be9c6d9509
SergeyVolkoff <vol7745@yandex.ru>
2023-01-06 10:34:52 -0500

    sfd

--- Avtomat/BM10/clss_Router.py
+++ Avtomat/BM10/clss_Router.py
@@ -451,8 +451,8 @@ if __name__ == "__main__":
             #print(r1.show_int3G(device,"uci show network | grep LTE"))     # Show LTE
             #print(r1.cfg_LTE(device,r1.commands_cfg_3G))                   # Cfg LTE
             #print(r1.cfg_pass(device,commands="passwd"))                   # Cfg pass
-            print(r1.vlan_cfg(device,r1.commands_vlan_cfg))                # Cfg vlan
-            #print(r1.base_cfg(device, r1.commands_base_cfg))               # Cfg base_cfg (wan-st_ip, fire,name)
+            #print(r1.vlan_cfg(device,r1.commands_vlan_cfg))                # Cfg vlan
+            print(r1.base_cfg(device, r1.commands_base_cfg))               # Cfg base_cfg (wan-st_ip, fire,name)
             #print (r1.base_802_cfg(device, r1.commands_802_1d_cfg))        # Cfg for 802d (STP)
             #print (r1.base_cfg(device, r1.commands_dmz_cfg))               # Cfg for DMZ
             #print (r1.base_cfg(device, r1.commands_gre_config))            # Cfg for test GRE
