7f786faee8220b79525351d3b204bd085a8a95ad
SergeyVolkoff <vol7745@yandex.ru>
2023-05-11 03:50:00 -0400

    test_base_OK

--- Avtomat/BM10/test_base_cfg.py
+++ Avtomat/BM10/test_base_cfg.py
@@ -10,9 +10,11 @@ from netmiko import (
 from clss_Router import Router
 from check_base_cfg import check_sup_ASIC, check_firewall,check_name_dev
 
-def test_base_cfg():
+def test_enable_support_ASIC():
     assert check_sup_ASIC("uci show firewall.@defaults[0].flow_offloading_hw")==True, "Firewall offloading_hw  disable"
+def test_enable_firewall_zone_wan():
     assert check_firewall("uci show firewall.@zone[1].forward")==True, "Firewall.@zone[1].forward disable"
+def test_enable_name_device():
     assert check_name_dev("uci show system.@system[0].hostname")==True, "Name device is wrong"
     # assert check_base_cfg("uci show system.@system[0].hostname") ==True, "Name device is wrong"
 # def test_name_device():
