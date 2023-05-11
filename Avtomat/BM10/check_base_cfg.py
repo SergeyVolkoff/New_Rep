7eef686a9e5b47fd66705b9363dd35dd9d8e2e34
SergeyVolkoff <vol7745@yandex.ru>
2023-05-11 03:30:48 -0400

    test_base_OK

--- Avtomat/BM10/check_base_cfg.py
+++ Avtomat/BM10/check_base_cfg.py
@@ -11,17 +11,35 @@ from clss_Router import Router
 Из всего базового конфига проверяется только та команда, что вводится в test.py 
 Чек заточен на проверку включен ли аппаратный ускоритель в файервол
 """
-def check_base_cfg(comm):
-    with open("BM10_LTE.yaml")as f:
-        temp = yaml.safe_load(f)
-        for t in temp:
-            device = dict(t)
-            r1 = Router(**device)
+with open("BM10_LTE.yaml") as f:
+    temp = yaml.safe_load(f)
+    for t in temp:
+        device = dict(t)
+        r1 = Router(**device)
+def check_sup_ASIC(comm):
     try:
         temp = r1.send_sh_command(device, comm)
+        #print(temp)
         if "offloading_hw='1'" in temp:
             return True
-        if "forward='ACCEPT'" in temp:
-            return  True
     except ValueError as err:
         return False
+def check_firewall(comm):
+    try:
+        temp = r1.send_sh_command(device,comm)
+        if "forward='ACCEPT'" in temp:
+            return True
+    except ValueError as err:
+        return False
+def check_name_dev(comm):
+    try:
+        temp = r1.send_sh_command(device, comm)
+        if "DUT_7621" in temp:
+            return True
+
+
+    except ValueError as err:
+        return False
+# if __name__ == "__main__":
+#      result = check_base_cfg("uci show")
+#      print(result)
--- Avtomat/BM10/test_base_cfg.py
+++ Avtomat/BM10/test_base_cfg.py
@@ -8,9 +8,12 @@ from netmiko import (
     NetmikoAuthenticationException,
 )
 from clss_Router import Router
-from check_base_cfg import  check_base_cfg
+from check_base_cfg import check_sup_ASIC, check_firewall,check_name_dev
 
 def test_base_cfg():
-    assert check_base_cfg("uci show firewall.@defaults[0].flow_offloading_hw")==True, "Firewall offloading_hw  disable"
-    assert check_base_cfg("uci show firewall.@zone[1].forward")==True, "Firewall.@zone[1].forward disable"
-
+    assert check_sup_ASIC("uci show firewall.@defaults[0].flow_offloading_hw")==True, "Firewall offloading_hw  disable"
+    assert check_firewall("uci show firewall.@zone[1].forward")==True, "Firewall.@zone[1].forward disable"
+    assert check_name_dev("uci show system.@system[0].hostname")==True, "Name device is wrong"
+    # assert check_base_cfg("uci show system.@system[0].hostname") ==True, "Name device is wrong"
+# def test_name_device():
+#     assert check_name_device("uci show system.@system[0].hostname")=True, "sdf"
