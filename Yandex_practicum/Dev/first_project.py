839ef0740f434af5839ac0c84aeeb63c2b657a99
SergeyVolkoff <vol7745@yandex.ru>
2023-07-18 09:48:07 -0400

    sd

--- Yandex_practicum/Dev/first_project.py
+++ Yandex_practicum/Dev/first_project.py
@@ -1,1 +1,12 @@
-sdf
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.field_names = ['Name', 'Age', 'City']
+table.add_row(['Стёпа', 11, 'Москва'])
+table.add_row(['Андрей', 7, 'Ковров'])
+table.add_row(['Тоня', 6, 'Минск'])
+table.add_row(['Толя', 8, 'Санкт-Петербург'])
+table.add_row(['Лера', 5, 'Краснодар'])
+
+print(table) 
