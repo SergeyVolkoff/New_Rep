0f38b7b621c4d6010c4999103a15a18e2e86f556
SergeyVolkoff <vol7745@yandex.ru>
2023-07-27 10:54:05 -0400

    test_param

--- Avtomat/BM10/test_check_pppoe_client.py
+++ Avtomat/BM10/test_check_pppoe_client.py
@@ -22,7 +22,9 @@ def test_check_ping_inet():
 def test_check_ip_pppoe():
 
     assert check_ip_pppoe('ip a')==True, "interface exist, but dont have ip, tunnel state DOWN"
-
+"""
+В блоке 28-51 используется параметризация с использованием Task
+"""
 # @pytest.mark.parametrize('summary',
 #                          [('192.168.4.1'),
 #                          ('192.168.3.1'),
@@ -34,16 +36,23 @@ def test_check_ip_pppoe():
 #     assert equivalent (check_ip_peer("ip a"), task)== True, "Wrong ip"
 
 
-tasks_to_check_ip = (
+tasks_to_check_ip = ( # заносим в переменную данные для проверки
     Task('192.168.4.1'),
     Task('192.168.2.1')
 )
-task_ids = ['Task({})'.format(t.summary)
+task_ids = ['Task({})'.format(t.summary) # определям параметр ids чтобы сделать идентификаторы для понимания вывода теста
             for t in tasks_to_check_ip
             ]
 @pytest.mark.parametrize("task",tasks_to_check_ip, ids=task_ids)
+# используем параметризацию,
+# передаем в нее первый аргумент parametrize() — это строка с разделенным запятыми списком имен — 'task' в нашем случае,
+# переменную указывающую на данные для проверки (tasks_to_check_ip) и ids
 def test_check_ip_peer(task):
-    #task = Task(task)
+    # функция вызывается многократно  с указанными аргументами task
+    # task = Task(task)
     assert equivalent(check_ip_peer("ip a"), task) == True, "Wrong ip"
+    # assert вызовет функцию equivalent которая проверит
+    # равенство на соответствие, используя значение (True\Фолс), полученное из check_ip_peer,
+    # а потом проверит внутри assertа на равенство с Труе и вернет резулььтат
 def equivalent (t1,t2):
     return (t1== t2.summary)
