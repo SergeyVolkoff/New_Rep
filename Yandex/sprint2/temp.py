9c762a4a490e5ecccf87bbf29a5329be276a3007
SergeyVolkoff <vol7745@yandex.ru>
2023-08-01 10:40:02 -0400

    ya

--- Yandex/sprint2/temp.py
+++ Yandex/sprint2/temp.py
@@ -1,49 +1,70 @@
-class Bird:
-    def __init__(self, name, size):
+class Human:
+    def __init__(self, name):
         self.name = name
-        self.size = size
 
-    def describe(self, full=False):
-        return f'Размер птицы {self.name} — {self.size}.'
+    # ответ по умолчанию для всех одинаковый, можно
+    # доверить его родительскому классу
+    def answer_question(self, question):
+        print('Очень интересный вопрос! Не знаю.')
 
+class Student(Human):
+    def __init__(self, name):
+        super().__init__(name)
+    #  метод ask_question() принимает параметр someone:
+    #  это объект, экземпляр класса Curator, Mentor или CodeReviewer,
+    #  которому Student задаёт вопрос;
+    #  параметр question — это просто строка
+    #  имя объекта и текст вопроса задаются при вызове метода ask_question
+    def ask_question(self, some, question):
+        # напечатайте на экран вопрос в нужном формате
+        print(f'{some.name}, {question}')
+        # запросите ответ на вопрос у someone
+        some.answer_question(question)
+        print()  # этот print выводит разделительную пустую строку
 
-class Parrot(Bird):
-    def __init__(self, name, size, color):
-        super().__init__(name, size)
-        self.color = color
-
-    # Переопределите метод describe().
-    def describe(self, full=False):
-        if full == False:
-            return Bird.describe(self)
+class Curator(Human):
+    def __init__(self, name):
+        super().__init__(name)
+    def answer_question(self, question):
+        # здесь нужно проверить, пришёл куратору знакомый вопрос или нет
+        # если да - ответить на него
+        # если нет - вызвать метод answer_question() у родительского класса
+        if question in 'мне грустненько, что делать?':
+            print('Держись, всё получится. Хочешь видео с котиками?')
         else:
-            if full == True:
-                return (f'Попугай {self.name} — заметная птица, окрас её перьев — {self.color},'
-                        f'а размер — {self.size}. Интересный факт: попугаи чувствуют ритм, '
-                        f'а вовсе не бездумно двигаются под музыку. Если сменить композицию,'
-                        f' то и темп движений птицы изменится.')
+            return super().answer_question(question)
+# объявите и реализуйте классы CodeReviewer и Mentor
 
-
-class Penguin(Bird):
-    def __init__(self, name, size, genus):
-        super().__init__(name, size)
-        self.genus = genus
-
-    # Переопределите метод describe().
-    def describe(self, full=False):
-        if full == False:
-            return Bird.describe(self)
+class CodeReviewer(Human):
+    def __init__(self, name):
+        super().__init__(name)
+    def answer_question(self, question):
+        if question in 'что не так с моим проектом?':
+            print('О, вопрос про проект, это я люблю.')
         else:
-            if full == True:
-                return (f'Размер пингвина {self.name} из рода genus — {self.size}. '
-                        f'Интересный факт: однажды группа геологов-разведчиков похитила пингвинье яйцо, '
-                        f'и их принялась преследовать вся стая, не пытаясь, впрочем, при этом нападать. '
-                        f'Посовещавшись, похитители вернули птицам яйцо, и те отстали.')
+            return super().answer_question(question)
 
+class Mentor(Human):
+    def __init__(self, name):
+        super().__init__(name)
+    def answer_question(self, question):
+        if question in 'как устроиться работать питонистом?':
+            print ('Сейчас расскажу.')
+        elif question in 'мне грустненько, что делать?':
+            print ('Отдохни и возвращайся с вопросами по теории.')
+        else:
+            return super().answer_question(question)
 
-kesha = Parrot('Ара', 'средний', 'красный')
-kowalski = Penguin('Королевский', 'большой', 'Aptenodytes')
+# следующий код менять не нужно, он работает, мы проверяли
+student1 = Student('Тимофей')
+curator = Curator('Марина')
+mentor = Mentor('Ира')
+reviewer = CodeReviewer('Евгений')
+friend = Human('Виталя')
 
-# Вызов метода у созданных объектов.
-print(kesha.describe())
-print(kowalski.describe(True))
+student1.ask_question(curator, 'мне грустненько, что делать?')
+student1.ask_question(mentor, 'мне грустненько, что делать?')
+student1.ask_question(reviewer, 'когда каникулы?')
+student1.ask_question(reviewer, 'что не так с моим проектом?')
+student1.ask_question(friend, 'как устроиться на работу питонистом?')
+student1.ask_question(mentor, 'как устроиться работать питонистом?')
